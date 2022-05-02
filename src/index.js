3645dd6b1c428cc1882bf19ff04afcaa3f81a931
meera-raveendran <100879326+meera-raveendran@users.noreply.github.com>
2022-05-02 12:20:41 -0700

    added icon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 let apiKey="a2e0cfbfe5d276ae777464db6e6424f7"
 let unit="metric";
-let city="New York";
+let city="texas";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
 axios.get(apiUrl).then(showTemperature);
 function showTemperature(response){
@@ -8,20 +8,23 @@ function showTemperature(response){
    let temperature=document.querySelector("#tempElement")
    let date=document.querySelector(".weatherDate");
    let description=document.querySelector(".weatherDesc");
-   let percipitation=document.querySelector(".percipitation");
-   let humidity=document.querySelector(".humidity");
-   let windspeed=document.querySelector(".windspeed");
+   let percipitation=document.querySelector("#percipitation");
+   let humidity=document.querySelector("#humidity");
+   let windspeed=document.querySelector("#windspeed");
    let h1 = document.querySelector(".weatherCity");
+   let iconElement=document.querySelector("#icon")
   h1.innerHTML = response.data.name;
-    date.innerHTML=formatDate(response.data.dt*1000);   
+    date.innerHTML=formatDate(response.data.dt *1000);   
    temperature.innerHTML=Math.round(response.data.main.temp);
    description.innerHTML=response.data.weather[0].description;
    let currentPercip=response.data.wind.deg;
-   percipitation.innerHTML=`percipitation:${currentPercip}%`;
+   percipitation.innerHTML=`${currentPercip}`;
    let currentHumid=response.data.main.humidity;
-   humidity.innerHTML=`Humidity:${currentHumid}`;
-   let currentWind=response.data.wind.speed;
-   windspeed.innerHTML=`Windspeed:${currentWind}`;
+   humidity.innerHTML=`${currentHumid}`;
+   let currentWind=Math.round(response.data.wind.speed);
+   windspeed.innerHTML=`${currentWind}`;
+   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+   iconElement.setAttribute("alt",response.data.weather[0].description);
 };
 function formatDate(timeStamp){
    
